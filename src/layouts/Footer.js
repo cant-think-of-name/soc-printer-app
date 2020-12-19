40eaf819e51a4c6e93969c5e6fdeb4dd42844ad2
EvonDong <e0322896@u.nus.edu>
2020-12-19 23:58:49 +0800

    Made changes to footer after feedback

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-import { Paper, withStyles, Grid, TextField, Button, AppBar, Typography, Toolbar, CssBaseline} from '@material-ui/core';
+import { Paper, withStyles, Grid, TextField, Button, AppBar, Typography, Toolbar, CssBaseline, Container } from '@material-ui/core';
 
 function Copyright() {
   return (
@@ -17,7 +15,7 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = theme => ({
   root: {
     display: 'flex',
     flexDirection: 'auto',
@@ -34,10 +32,8 @@ const useStyles = makeStyles((theme) => ({
     bottom: "0",
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
   },
-}));
+});
 
 
 
