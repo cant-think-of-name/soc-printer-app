704d3024275a24223d7b5d77e307e8e0e66fc0a4
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-19 12:27:27 +0800

    Set up an initial theme

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import {createMuiTheme} from "@material-ui/core/styles"
+
+const theme = createMuiTheme({
+  "palette": {
+    "common": {
+      "black": "#000",
+      "white": "#fff"
+    }
+  }
+})
+
+export default theme
