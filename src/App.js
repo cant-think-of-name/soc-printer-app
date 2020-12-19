5356ea41fa2ac29260615c54bad111db43c4ef64
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-19 12:00:32 +0800

    Set up app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import Header from "./layouts/Header"
+import Body from "./layouts/Body"
+import Footer from "./layouts/Footer"
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
