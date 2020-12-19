1715ff960635c744de0ddd385bae0a67e61dca29
Lim Kang Yee <kangyee2408@gmail.com>
2020-12-19 20:58:16 +0800

    Designed printing status pages.

--- backend/controller/WebConsole.js
+++ backend/controller/WebConsole.js
@@ -1,6 +1,7 @@
 const { NodeSSH } = require("node-ssh");
 const ssh = new NodeSSH();
 
+<<<<<<< HEAD
 module.exports = async (ws, req) => {
   ws.on("message", async msg => {
     // eslint-disable-next-line default-case
@@ -22,6 +23,80 @@ module.exports = async (ws, req) => {
         }
         break;
       case "command":
+=======
+/*const sshConfig = {
+  host: "<host name here>",
+  username: "<username here>",
+  password: "<password>"
+};*/
+
+/*class WebConsole {
+  constructor() {
+    this.sshConfig = {
+      host: "sunfire-r.comp.nus.edu.sg",
+      username: "",
+      password: ""
+    }
+  }
+
+  async login(ws, req) {
+    const data = JSON.parse(msg);
+    this.sshConfig = {
+      host: "sunfire-r.comp.nus.edu.sg",
+      username: `${data.username}`,
+      password: `${data.password}`
+    }
+  }
+
+  async message(ws, req) {
+    this.sshConfig = {
+      host: "sunfire-r.comp.nus.edu.sg",
+      username: `${data.username}`,
+      password: `${data.password}`
+    }
+    await ssh.connect(this.sshConfig);
+    const shellStream = await ssh.requestShell();
+    ws.on("message", msg => {
+      const data = JSON.parse(msg);
+      switch (data.method) {
+        case "command":
+          shellStream.write(data.command.trim() + "\n");
+          break;
+        }
+    });
+
+    // listener
+    shellStream.on("data", data => {
+      const d = JSON.stringify({
+        jsonrpc: "2.0",
+        data: data.toString()
+      });
+      ws.send(d);
+      console.log("replied");
+    });
+
+    shellStream.stderr.on("data", data => {
+      const d = JSON.stringify({
+        jsonrpc: "1.0",
+        data: data.toString()
+      });
+      ws.send(d);
+    });
+  }
+}
+
+module.exports = WebConsole;*/
+
+module.exports = async (ws, req) => {
+
+  ws.on("message", async msg => {
+
+    switch (data.method) {
+
+      case "command":
+        const data = JSON.parse(msg);
+
+>>>>>>> Designed printing status pages.
         sshConfig = {
           host: "sunfire-r.comp.nus.edu.sg",
           username: `${data.username}`,
