690ca63ea65e5503e7bc379bca3d86e46f5470fe
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-20 00:44:23 +0800

    Create a login method on the websocket connection

--- backend/controller/WebConsole.js
+++ backend/controller/WebConsole.js
@@ -1,78 +1,27 @@
 const { NodeSSH } = require("node-ssh");
 const ssh = new NodeSSH();
 
-/*const sshConfig = {
-  host: "<host name here>",
-  username: "<username here>",
-  password: "<password>"
-};*/
-
-/*class WebConsole {
-  constructor() {
-    this.sshConfig = {
-      host: "sunfire-r.comp.nus.edu.sg",
-      username: "",
-      password: ""
-    }
-  }
-
-  async login(ws, req) {
-    const data = JSON.parse(msg);
-    this.sshConfig = {
-      host: "sunfire-r.comp.nus.edu.sg",
-      username: `${data.username}`,
-      password: `${data.password}`
-    }
-  }
-
-  async message(ws, req) {
-    this.sshConfig = {
-      host: "sunfire-r.comp.nus.edu.sg",
-      username: `${data.username}`,
-      password: `${data.password}`
-    }
-    await ssh.connect(this.sshConfig);
-    const shellStream = await ssh.requestShell();
-    ws.on("message", msg => {
-      const data = JSON.parse(msg);
-      switch (data.method) {
-        case "command":
-          shellStream.write(data.command.trim() + "\n");
-          break;
-        }
-    });
-
-    // listener
-    shellStream.on("data", data => {
-      const d = JSON.stringify({
-        jsonrpc: "2.0",
-        data: data.toString()
-      });
-      ws.send(d);
-      console.log("replied");
-    });
-
-    shellStream.stderr.on("data", data => {
-      const d = JSON.stringify({
-        jsonrpc: "1.0",
-        data: data.toString()
-      });
-      ws.send(d);
-    });
-  }
-}
-
-module.exports = WebConsole;*/
-
 module.exports = async (ws, req) => {
-
   ws.on("message", async msg => {
-
+    // eslint-disable-next-line default-case
+    const data = JSON.parse(msg)
     switch (data.method) {
-
+      case "login":
+        const sshConfig = {
+          host: "sunfire-r.comp.nus.edu.sg",
+          username: `${data.username}`,
+          password: `${data.password}`
+        }
+        const result = await ssh.connect(sshConfig);
+        if (result.isConnected()) {
+          const response = JSON.stringify({
+            jsonrpc: "2.0",
+            isConnected: true
+          })
+          ws.send(response)
+        }
+        break;
       case "command":
-        const data = JSON.parse(msg);
-
         sshConfig = {
           host: "sunfire-r.comp.nus.edu.sg",
           username: `${data.username}`,
