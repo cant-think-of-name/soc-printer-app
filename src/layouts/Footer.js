f409b791778bbae0260d248ca62f72b2cdc10e92
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-20 09:06:42 +0800

    Clean up footer styling

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -24,11 +24,6 @@ const styles = (theme) => ({
     bottom: "0",
     width: "100%",
   },
-  links: {
-    display: 'flex',
-    flexDirection: 'auto',
-    minHeight: '100vh',
-  },
   main: {
     display: 'flex',
     flexDirection: 'row',
@@ -43,90 +38,53 @@ const styles = (theme) => ({
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
   },
-}));
-
-
-
-/**
- * For the footer
- * @param {} props 
- */
-
-var style = {
-  backgroundColor: "#F8F8F8",
-  borderTop: "1px solid #E7E7E7",
-  textAlign: "center",
-  padding: "20px",
-  position: "fixed",
-  left: "0",
-  bottom: "0",
-  width: "100%",
-}
+});
 
 class Footer extends React.Component {
   render() {
     const { classes } = this.props;
 
     return (
-      <div>
-          <Box style={style}>
-            <Container component="main" className={classes.main}>
-              <Typography variant="h5" component="h1" gutterBottom>
-                Credits to:
-              </Typography>
-              <Box display="flex" justifyContent="space-between">
-                <Box display="flex" flexDirection="column">
-                    Chow Jia Ying:
-                    <Typography className={classes.root}>
-                      <Link href="https://www.github.com/C-likethis123" >
-                        GitHub page
-                      </Link>
-                    </Typography>
-                    <Typography className={classes.root}>
-                      <Link href="https://c-likethis123.github.io/website/" >
-                        Personal page
-                      </Link>
-                    </Typography>
-                </Box>
-                <Box display="flex" flexDirection="column">
-                    Lim Kang Yee:
-                  <Typography className={classes.root}>
-                    <Link href="https://github.com/kangyeelim" >
-                      GitHub page
-                    </Link>
-                  </Typography>
-                  <Typography className={classes.root}>
-                    <Link href="https://kangyeelim.github.io/aboutme/" >
-                      Personal page
-                    </Link>
-                  </Typography>
-                </Box>
-                <Box display="flex" flexDirection="column">
-                  Evon Dong Bing Bing:
-                  <Typography className={classes.root}>
-                    <Link href="https://github.com/EvonDong" >
-                      GitHub page
-                    </Link>
-                  </Typography>
-                </Box>
-                <Box display="flex" flexDirection="column">
-                  Michaela Tang:
-                  <Typography className={classes.root}>
-                    <Link href="https://github.com/MichaelaTSH" >
-                      GitHub page
-                    </Link>
-                  </Typography>
-                </Box>
+      <Box className={classes.root}>
+        <footer className={classes.footer} >
+          <Container>
+              Credits to:
+            <Box display="flex" justifyContent="space-between">
+              <Box display="flex" flexDirection="column">
+                Chow Jia Ying:
+                <Link href="https://www.github.com/C-likethis123" >
+                  GitHub page
+                </Link>
+                <Link href="https://c-likethis123.github.io/website/" >
+                  Personal page
+                </Link>
+              </Box>
+              <Box display="flex" flexDirection="column">
+                Lim Kang Yee:
+                <Link href="https://github.com/kangyeelim" >
+                  GitHub page
+                </Link>
+                <Link href="https://kangyeelim.github.io/aboutme/" >
+                  Personal page
+                </Link>
+              </Box>
+              <Box display="flex" flexDirection="column">
+                Evon Dong Bing Bing:
+                <Link href="https://github.com/EvonDong" >
+                  GitHub page
+                </Link>
+              </Box>
+              <Box display="flex" flexDirection="column">
+                Michaela Tang:
+                  <Link href="https://github.com/MichaelaTSH" >
+                    GitHub page
+                  </Link>
+              </Box>
             </Box>
+            <Copyright />
           </Container>
-            <footer className={classes.footer} >
-              <Container maxWidth="sm">
-                <Typography variant="body1">My sticky footer can be found here.</Typography>
-                <Copyright />
-              </Container>
-            </footer>
-          </div>
-      </div>
+        </footer>
+      </Box>
   )
 }
 }
