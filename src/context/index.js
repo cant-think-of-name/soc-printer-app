8919460c27df1c796201f8950acdd1284f9fba69
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-20 10:45:13 +0800

    Create a WebSocketClient to manage websocket connections

--- src/context/index.js
+++ src/context/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { w3cwebsocket as WebSocket } from "websocket"
 
-const ws = new WebSocket("ws://localhost:5000", "echo-protocol")
-const StateContext = React.createContext(ws)
+class WebSocketClient {
+  constructor() {
+    this.ws = new WebSocket("ws://localhost:5000", "echo-protocol")
+  }
+
+  connect(username, password) {
+    const webSocket = this.ws
+    const loginMessage = JSON.stringify({
+      method: "login",
+      username,
+      password,
+    })
+    webSocket.send(loginMessage)
+    return new Promise((resolve, reject) => {
+      webSocket.onmessage = (msg) => {
+        const {data} = msg
+        const {isConnected} = JSON.parse(data)
+        if (isConnected) {
+          resolve()
+        } else {
+          reject()
+        }
+      }
+    })
+  }
+}
+const StateContext = React.createContext(new WebSocketClient())
 
 export default StateContext
