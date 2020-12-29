2a471a8c85e88a89f29255c8063907dd764ccaae
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-29 09:58:54 +0800

    Lint other packages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Header from "./layouts/Header"
-import Body from "./layouts/Body"
-import Footer from "./layouts/Footer"
+import Header from "./layouts/Header";
+import Body from "./layouts/Body";
+import Footer from "./layouts/Footer";
 
-import { CssBaseline } from "@material-ui/core"
-import { ThemeProvider } from '@material-ui/core/styles'
-import theme from "./theme"
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from "./theme";
 
 function App() {
   return (
