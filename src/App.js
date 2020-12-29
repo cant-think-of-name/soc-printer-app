8dd07cd0e89ca80bc6ed237663a04c8dc1dd48a1
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-29 10:27:33 +0800

    testing 2

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Header from "./layouts/Header";
 import Body from "./layouts/Body";
 import Footer from "./layouts/Footer";
 
-import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from "./theme";
+import { CssBaseline } from "@material-ui/core"
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from "./theme"
 
 function App() {
   return (
